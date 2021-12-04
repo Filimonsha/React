f9dde696633ede755a866b68e83ec34afc20b9eb
Filimonsha <Filimonshacs@mail.ru>
2021-12-04 13:15:42 +0300

    Add header Hello React

---
+++ src/header.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import style from './header.css'
+export function Header(){
+    return (
+        <header>
+            <h1 className={style.example}>Hello React</h1>
+        </header>
+    )
+}
